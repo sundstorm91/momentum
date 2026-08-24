83979bee42b24b3547f97bcb65204fb07efa75f0
Aleksandr <118198548+sundstorm91@users.noreply.github.com>
2026-08-24 22:04:24 +0700

    fix: correct audio file paths for GitHub Pages

--- js/playList.js
+++ js/playList.js
@@ -1,38 +1,38 @@
 const playList = [
     {
       title: 'AK - Discovery',
-      src: '../assets/sounds/AK-Discovery.mp3',
+      src: 'assets/sounds/AK-Discovery.mp3',
       /* duration: '03:34' */
       state: false,
     },
     {
       title: 'AK   - Life after death',
-      src: '../assets/sounds/AK-LifeAfterDeath.mp3',
+      src: 'assets/sounds/AK-LifeAfterDeath.mp3',
       /* duration: '06:01' */
       state: false,
 
     },
     {
       title: 'Astral - Nomyn',
-      src: '../assets/sounds/Nomy.mp3',
+      src: 'assets/sounds/Nomy.mp3',
       /* duration: '05:39' */
       state: false,
     },
     {
       title: 'CMA - Forgive',
-      src: '../assets/sounds/Cma-Forgive.mp3',
+      src: 'assets/sounds/Cma-Forgive.mp3',
       /* duration: '03:10' */
       state: false,
     },
     {
         title: 'Manu Chao - Me gustas tu',
-        src: '../assets/sounds/ManuChao-Megustastu.mp3',
+        src: 'assets/sounds/ManuChao-Megustastu.mp3',
         /* duration: '01:43' */
         state: false,
     },
     {
         title: 'Kaito Shoma - Hotline',
-        src: '../assets/sounds/Shoma-Hotline.mp3',
+        src: 'assets/sounds/Shoma-Hotline.mp3',
         /* duration: '02:38' */
         state: false,
     },
